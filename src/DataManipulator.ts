0e85c40048ab9c739c6cf8fc4d27f2a0551b3365
sp026 <srish.popuri@gmail.com>
2024-08-10 07:26:18 -0400

    Update DataManipulator.tsx
    - updated Row interface with appropriate variables
    
    - updated serverResponds func with making & calculating needed variables
     -- updated its return values too

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,19 +1,33 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number,
+  timestamp: Date,           // need ot match new elements (as we did in Graph.tsx)
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+    return serverResponds.map((el: any) => {  // created variables and accessed serverResponse to calculate prices / ratios & bounds
+      const priceABC = (serverRespond[0].top_ask_price + serverRespond[0].top_bid_price) /2;
+      const priceDEF = (serverRespond[1].top_ask_price + serverRespond[0].top_bid_price) /2;
+      const ratio = priceABC/priceDEF;
+      const upperBound = 1 + 0.1;
+      const lowerBound = 1 - 0.1;
+
+      return {  // updated return values appropriatley to return what we need
+        price_abc: priceABC,
+        price_def: priceDEF,
+        ratio,
+        upper_bound: upperBound,
+        lower_bound: lowerBound,
+        trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio: undefined,
+        timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? serverRespond[0].timestamp : serverRespond[1].timestamp,
       };
     })
   }
